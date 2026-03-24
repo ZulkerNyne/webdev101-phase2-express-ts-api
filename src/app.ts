4ed9dd65a050c42b25fc026d17c0f2a4fb2070b3
MD ZULKER NYNE <mdzulkernyne@gmail.com>
2026-03-24 15:05:55 -0600

    phase2: lesson 2.3 add request logging middleware and correct middleware order

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
 
 import {router} from "./routes";
+import {requestLogger} from "./middleware/requestLogger";
 export const app = express();
+//parse JSON first(so req.body is available later)
 app.use(express.json());
+//log requests
+app.use(requestLogger);
+//routes
 app.use(router);
